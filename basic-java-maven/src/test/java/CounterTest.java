ef6ff5343ba6db53425a9326ce7245c2a25116cb
fadliachsan27 <fadliachsan010@gmail.com>
2026-05-17 19:44:09 +0700

    Add files via upload

---
+++ basic-java-maven/src/test/java/CounterTest.java
@@ -0,0 +1,39 @@
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class CounterTest {
+
+    @Test
+    public void testReset() {
+        Counter testCounter = new Counter();
+
+        for (int i = 0; i < 10; i++) {
+            testCounter.increment();
+        }
+
+        testCounter.reset();
+
+        assertEquals(0, testCounter.getCount());
+    }
+
+    @Test
+    public void testIncrement() {
+        Counter testCounter = new Counter();
+
+        for (int i = 1; i < 10; i++) {
+            testCounter.increment();
+            assertEquals(i, testCounter.getCount());
+        }
+    }
+
+    @Test
+    public void testDecrement() {
+        Counter testCounter = new Counter();
+
+        for (int i = 1; i < 10; i++) {
+            testCounter.decrement();
+            assertEquals(i * -1, testCounter.getCount());
+        }
+    }
+
+}
